61970d2803108a5732169032c8a639423159878e
sergiomartinez_1997@hotmail.com <sergiomartinez_1997@hotmail.com>
2020-11-15 19:14:10 +0100

    Added Manager login screen

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import MainScreen from './screens/MainScreen';
 import TabTwoScreen from './screens/TabTwoScreen';
+import ManagerLogin from './screens/ManagerLogin';
 
 const Drawer = createDrawerNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
       <Drawer.Navigator initialRouteName="MainScreen">
         <Drawer.Screen name="MainScreen" component={MainScreen} />
         <Drawer.Screen name="TabTwoScreen" component={TabTwoScreen} />
+        <Drawer.Screen name="Manager Login" component={ManagerLogin} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
